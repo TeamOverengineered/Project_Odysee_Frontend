a4fe039c64e0d8aeb38919e9826a7b92f151569c
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-11-03 12:16:12 +0100

    add footer

---
+++ components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="w-full bg-base-300 h-20 fixed bottom-0 flex justify-between px-4 items-center">
+        <div>© TeamOverengineered</div>
+        <a href="/impressum">Impressum</a>
+        <a href="/datenschutz">Datenschutz</a>
+    </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,15 @@
 <template>
     <Header />
-    <slot />
+    <div class="grid grid-cols-5">
+        <div class="col-span-1">
+
+        </div>
+        <div class="col-span-3">
+            <slot />
+        </div>
+        <div class="col-span-1">
+
+        </div>
+    </div>
+    <Footer />
 </template>
