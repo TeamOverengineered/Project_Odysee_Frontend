24067173e4e85f3b41b37dc145a9ff8222c55390
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-11-09 08:36:52 +0100

    add menu images

--- components/menu.vue
+++ components/menu.vue
@@ -2,15 +2,15 @@
     <div class="h-full w-64 bg-base-300 flex flex-col justify-between">
         <div>
             <ul class="items-center">
-                <a href=""><li class="flex items-center border border-gray-400 h-12 hover:bg-base-200">Dashboard</li></a>
-                <a href=""><li class="flex items-center border border-t-0 border-gray-400 h-12 hover:bg-base-200">SharedDesk</li></a>
-                <a href=""><li class="flex items-center border border-t-0 border-gray-400 h-12 hover:bg-base-200">Tickets</li></a>
-                <a href=""><li class="flex items-center border border-t-0 border-gray-400 h-12 hover:bg-base-200">Nachrichten</li></a>
+                <a href=""><li class="flex items-center border border-gray-400 h-12 hover:bg-base-200"><img class="mx-2" src="/images/dashboard-logo.png" alt="Dashboard">Dashboard</li></a>
+                <a href=""><li class="flex items-center border border-t-0 border-gray-400 h-12 hover:bg-base-200"><img class="mx-2" src="/images/desk-chair.png" alt="Shared Desk">SharedDesk</li></a>
+                <a href=""><li class="flex items-center border border-t-0 border-gray-400 h-12 hover:bg-base-200"><img class="mx-2" src="/images/ticket.png" alt="Tickets">Tickets</li></a>
+                <a href=""><li class="flex items-center border border-t-0 border-gray-400 h-12 hover:bg-base-200"><img class="mx-2" src="/images/speech-bubble.png" alt="Nachrichten">Nachrichten</li></a>
             </ul>
         </div>
         <div>
             <ul>
-                <a href=""><li class="flex items-center border border-gray-400 h-12 hover:bg-base-200">Einstellungen</li></a>
+                <a href=""><li class="flex items-center border border-gray-400 h-12 hover:bg-base-200"><img class="mx-2" src="/images/settings.png" alt="Einstellungen">Einstellungen</li></a>
             </ul>
         </div>
     </div>
